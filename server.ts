740aba009e6d4d9187573373d96617a1320e5101
Ramiro Tepehua <rtcface@gmail.com>
2025-04-22 09:55:33 -0600

    Inicio del proyecto

---
+++ server.ts
@@ -0,0 +1,6 @@
+import {
+  McpServer,
+  ResourceTemplate,
+} from "@modelcontextprotocol/sdk/server/mcp.js";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+import { z } from "zod";
